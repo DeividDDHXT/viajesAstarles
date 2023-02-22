5bb2670d5a357948e089c548aec202d18ae5d80f
DeividDDHXT <jestmon02@gmail.com>
2023-02-22 11:50:14 -0600

    second commit

--- lib/screens/Descripcion_plase_screen.dart
+++ lib/screens/Descripcion_plase_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:viajes_astales/screens/reviw.dart';
 
 class Descripcion_plase_screen extends StatelessWidget {
   const Descripcion_plase_screen({super.key});
@@ -11,11 +12,11 @@ class Descripcion_plase_screen extends StatelessWidget {
       fontWeight: FontWeight.bold,
     );
     const descriptionText =
-        'Do sit est eiusmod et eiusmod officia aute fugiat.\n\n Aliquip adipisicing officia excepteur incididunt cillum dolore enim esse adipisicing ullamco duis sit.\n\n Nulla sint cillum sint laborum enim ad adipisicing aliqua exercitation velit irure pariatur mollit exercitation.\n\n Commodo incididunt officia nisi officia ullamco aliqua adipisicing. \n\n Ea pariatur minim duis eiusmod nulla. Magna nulla proident ad ipsum nostrud aute.\n\n Dolor consequat esse tempor aute aliqua amet consectetur.\n\n Enim aliqua minim consequat ullamco officia consequat consequat ad velit elit reprehenderit officia.\n\n Aliqua exercitation mollit aute anim eiusmod commodo. Do Lorem deserunt fugiat mollit ullamco dolore duis reprehenderit magna cupidatat veniam. \n\n Sit irure Lorem dolor magna mollit ex labore incididunt.Id sunt reprehenderit fugiat magna nostrud ad eiusmod. Veniam voluptate officia nulla ea enim proident exercitation Lorem adipisicing veniam officia consequat. Aliquip labore nostrud non cillum et duis quis dolor ullamco laboris in deserunt.';
+        'Do sit est eiusmod et eiusmod officia aute fugiat.\n\n Aliquip adipisicing ';
 
     final star = Container(
       margin: const EdgeInsets.only(
-        top: 323.0,
+        top: 100.0,
         right: 3.0,
       ),
       child: const Icon(
@@ -27,7 +28,7 @@ class Descripcion_plase_screen extends StatelessWidget {
       children: [
         Container(
           margin: const EdgeInsets.only(
-            top: 320.0,
+            top: 100.0,
             left: 20.0,
             right: 20.0,
           ),
@@ -53,7 +54,7 @@ class Descripcion_plase_screen extends StatelessWidget {
         descriptionText,
         style: GoogleFonts.lato(
           textStyle: const TextStyle(
-            fontSize: 15,
+            fontSize: 14,
           ),
         ),
       ),
@@ -63,6 +64,7 @@ class Descripcion_plase_screen extends StatelessWidget {
       children: [
         titleAndStars,
         description,
+        const Review(),
       ],
     );
   }
